1e7be903959ae61bffd1af8d457f73018fcea336
ivan <ivanpanferov2016@mail.ru>
2023-03-10 08:22:51 +0300

    homework4

--- Program.cs
+++ Program.cs
@@ -1,62 +1,101 @@
-﻿/* Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-14212 > нет;  12821 -> да; 23432 -> да    */
+﻿/*
+Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
 
-Console.WriteLine ("Введите 5-ти значное число: ");
-string number = Console.ReadLine();
-int len = number.Length;
+int numberA = ReadInt("Введите число A: ");
+int numberB = ReadInt("Введите число B: ");
+ToDegree(numberA, numberB);
 
-if (len == 5)
+
+// Функция возведения в степень
+void ToDegree(int a, int b)
 {
-    if (number[0] == number[4] && number [1] == number[3])
+    int result = 1;
+    for (int i = 1; i <= b; i++)
     {
-        Console.WriteLine(number + " Палиндром");
+        result = result * a;
     }
-
-    else
-    {
-        Console.WriteLine(number + " Не палиндром");
-    }
-}
-else
-{
-    Console.WriteLine ("Вы ввели НЕ 5-и значное число");
+    Console.WriteLine(result);
 }
 
-/* Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
-A (3,6,8); B (2,1,-7), -> 15.84; A (7,-5, 0); B (1,-1,9) -> 11.53 */
+// Функция ввода
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
 
-int x1 = ReadInt("Введите х для точки 1: ");
-int y1 = ReadInt("Введите y для точки 1: ");
-int z1 = ReadInt("Введите z для точки 1: ");
-int x2 = ReadInt("Введите х для точки 2: ");
-int y2 = ReadInt("Введите y для точки 2: ");
-int z2 = ReadInt("Введите z для точки 2: ");
 
-int A = x2 - x1;
-int B = y2 - y1;
-int C = z2 - z1;
 
-double length = Math.Sqrt(A * A + B * B + C * C);
-Console.WriteLine("Расстояние = " + length);
 
-// Метод
+/*
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int number = ReadInt("Введите число: ");
+int len = NumberLen(number);
+SumNumbers(number, len);
+
+
+// Функция ввода
 int ReadInt(string message)
 {
     Console.Write(message);
     return Convert.ToInt32(Console.ReadLine());
 }
 
-/* Задача 23
-Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-3 -> 1, 8, 27; 5 -> 1, 8, 27, 64, 125  */
-
-int number = ReadInt("Введите число: ");
-
-for (int i = 1; i <= number; i++)
+// Функция подсчета цифр в числе
+int NumberLen(int a)
 {
-    Console.Write($"{i * i * i}, ");
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
 }
 
+// Функция вывода суммы цифр в числе
+void SumNumbers(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
+}
+
+
+
+
+
+
+
+/*
+Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+5 -> [1, 2, 5, 7, 19]
+3 -> [6, 1, 33]
+*/
+
+int lenArray = ReadInt("Введите длинну массива: ");
+
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,9);
+    Console.Write(randomArray[i] + " ");
+}
+
+
+// Функция ввода
 int ReadInt(string message)
 {
     Console.Write(message);
